5387fdf9eb0d8b9fae6f8a9bbd1255b1ba658057
Jawwad Alam <jawwadalam@gmail.com>
2019-10-04 18:51:18 +0500

    Changes with Addition of View Model and Bindings with Notes Listing functionality Complete

---
+++ SinglePage/SinglePage/ViewModels/NotesViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SinglePage.ViewModels
+{
+    public class NotesViewModel : INotifyPropertyChanged
+    {
+        public NotesViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+
+                TheNote = string.Empty;
+            });
+
+            ClearCommand = new Command(() => {
+                TheNote = string.Empty;
+            });
+
+        }
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command SaveCommand {get; }
+        public Command ClearCommand { get; }
+    }
+}
